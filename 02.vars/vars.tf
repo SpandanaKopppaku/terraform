c7830a7f51ae81bc91f6e37c319b28069a5bd87a
spandanakoppaku <skoppaku@gmail.com>
2023-07-14 09:45:03 -0400

    Auto Push

--- 02.vars/vars.tf
+++ 02.vars/vars.tf
@@ -14,3 +14,15 @@ output "sample_var_op" {
 
 # A Variable can be accessed directly by calling it as var.variableName, but if you're accessing in between a set of strings, then you need to enclose them in quotes and it should referred using ${var.VarName}
 # In Terraform there is no concept of Single Quotes. 
+
+
+# List Varaibale
+variable "example_list" {
+    default = {
+        "AWS" ,
+        "DEVOPS" ,
+        "Terraform",
+        
+    }
+  
+}
