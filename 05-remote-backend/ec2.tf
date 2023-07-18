4532ac5cfb43c5ac4b6699234c1f27f5d5259034
spandanakoppaku <skoppaku@gmail.com>
2023-07-18 10:23:18 -0400

    Auto Push

---
+++ 05-remote-backend/ec2.tf
@@ -0,0 +1,11 @@
+# Creates EC2
+
+resource "aws_instance" "app_server" {
+  ami                     = "ami-0991b21e0a2692e74"
+  instance_type           = "t3.nano"
+
+
+tags = {
+    Name = "Linux_server_with_Remotebackend"
+  }
+}
