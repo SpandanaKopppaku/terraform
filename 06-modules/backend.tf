8f124df98dc4921aed81610c4a8d410e5ee2f93e
spandanakoppaku <skoppaku@gmail.com>
2023-07-19 19:44:21 -0400

    Auto Push

---
+++ 06-modules/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket         = "b54-tf-state"
+    key            = "05-remote/terraform.tfstate"     # This is like the folder path that you prefer giving
+    region         = "us-east-1"
+  }
+}
