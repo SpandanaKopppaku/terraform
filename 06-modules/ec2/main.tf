6bf219cd2b2185d3f9538dd42708b5bf5f503c5c
spandanakoppaku <skoppaku@gmail.com>
2023-07-19 19:48:26 -0400

    Auto Push

---
+++ 06-modules/ec2/main.tf
@@ -0,0 +1,6 @@
+resource "aws_instance" "app_server" {
+  ami                     = "ami-0991b21e0a2692e74"
+  instance_type           = "t3.nano"
+  vpc_security_group_ids  =[aws_security_group.allows_all.id]
+
+}
