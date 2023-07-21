d9f7753ae1a31792b38e126b8eab54313c603393
spandanakoppaku <skoppaku@gmail.com>
2023-07-21 11:05:59 -0400

    Auto Push

--- 07-roboshop-setup/ec2/route53.tf
+++ 07-roboshop-setup/ec2/route53.tf
@@ -1,7 +1,7 @@
-resource "aws_route53_record" "www" {
+resource "aws_route53_record" "component" {
   zone_id = "Z0909398U99YUHS0RTOO"
   name    = var.COMPONENT
   type    = "A"
   ttl     = 10
-  records = [aws_eip.lb.public_ip]
+  records = [aws_spot_instance_request.spot_worker.private_ip]
 }
