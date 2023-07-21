af37eb1c13cde27a052223a9b4a4094fa9bb8be8
spandanakoppaku <skoppaku@gmail.com>
2023-07-21 13:36:21 -0400

    Auto Push

--- 07-roboshop-setup/main.tf
+++ 07-roboshop-setup/main.tf
@@ -1,6 +1,13 @@
 module "frontend" {
     source      = "./ec2"
+
+    for_each = toset( ["Todd", "James", "Alice", "Dottie"] )
+    name     = each.key
     COMPONENT   = each.key
      // APP_VERSION = each.value.APP_VERSION
 
+}
+
+for_each = toset( ["Todd", "James", "Alice", "Dottie"] )
+  name     = each.key
 }
