5c9d8da66758fa586792b1414e85fd9ab6ca4497
spandanakoppaku <skoppaku@gmail.com>
2023-07-21 10:06:52 -0400

    Auto Push

--- 07-roboshop-setup/ec2/main.tf
+++ 07-roboshop-setup/ec2/main.tf
@@ -9,8 +9,8 @@ resource "aws_spot_instance_request" "spot_worker" {
     Name = var.COMPONENT
   }
 
-   # This will be executed on the top of the machine once it's created
-provisioner "remote-exec" {
+#   # This will be executed on the top of the machine once it's created
+  provisioner "remote-exec" {
 
     # connection block establishes connection to this
     connection {
@@ -21,16 +21,35 @@ provisioner "remote-exec" {
     }
 
     inline = [
-        "ansible-pull -U https://github.com/SpandanaKopppaku/ansible.git -e ENV=dev -e COMPONENT=${var.COMPONENT} -e APP_VERSION=${var.APP_VERSION} roboshop-pull.yml"
+      "ansible-pull -U https://github.com/b54-clouddevops/ansible.git -e ROOT_PASSWORD=RoboShop@1 -e ENV=dev -e COMPONENT=${var.COMPONENT} -e APP_VERSION=${var.APP_VERSION} roboshop-pull.yml"
     ]
-
+  }
 }
 
-}
+# resource "aws_instance" "sample" {
+#   ami                     = data.aws_ami.image.id
+#   instance_type           = "t3.nano"
+#   vpc_security_group_ids  = [var.sg]
 
-variable "sg" {}   # step 3 : Now to use this info, declare an empty varianble and use it ( in root module, we have declared " sg = var.sgid )
+#   # This will be executed on the top of the machine once it's created
+#   provisioner "remote-exec" {
 
-output "public_ip" {
-  value   = aws_instance.sample.public_ip
+#     # connection block establishes connection to this
+#     connection {
+#       type     = "ssh"
+#       user     = "centos"
+#       password = "DevOps321"
+#       host     = self.private_ip             # aws_instance.sample.private_ip : Use this only if your provisioner is outside the resource.
+#     }
 
-}
+#     inline = [
+#       "ansible-pull -U https://github.com/b54-clouddevops/ansible.git -e ENV=dev -e COMPONENT=${var.COMPONENT} -e APP_VERSION=${var.APP_VERSION} roboshop-pull.yml"
+#     ]
+#   }
+# }
+
+# variable "sg" {}   # step 3 : Now to use this info, declare an empty varianble and user it ( in root module, we have declared " sg = var.sgid )
+
+# output "public_ip" {
+#   value   = aws_instance.sample.public_ip
+# }
