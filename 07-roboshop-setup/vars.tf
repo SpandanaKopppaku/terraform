662c7521b872bc85b36dc0c6c2c688222deb7bcb
spandanakoppaku <skoppaku@gmail.com>
2023-07-21 13:22:48 -0400

    Auto Push

--- 07-roboshop-setup/vars.tf
+++ 07-roboshop-setup/vars.tf
@@ -3,34 +3,34 @@
  variable "COMPONENTS" {
     default = {
         mongodb = {
-            APP_VERSION = "null"
+            //APP_VERSION = "null"
         },
         catalogue = {
-            APP_VERSION = "0.0.8"
+            //APP_VERSION = "0.0.8"
         },
         redis = {
-            APP_VERSION = "null"
+           // APP_VERSION = "null"
         },
         user = {
-            APP_VERSION = "0.0.5"
+            //APP_VERSION = "0.0.5"
         },
         cart = {
-            APP_VERSION = "0.0.2"
+            //APP_VERSION = "0.0.2"
         },
         mysql = {
-            APP_VERSION = "null"
+            //APP_VERSION = "null"
         },
         shipping = {
-            APP_VERSION = "0.0.4"
+            //APP_VERSION = "0.0.4"
         },
         rabbitmq = {
-            APP_VERSION = "null"
+            //APP_VERSION = "null"
         },
         payment = {
-            APP_VERSION = "0.0.3"
+            //APP_VERSION = "0.0.3"
         },
         frontend = {
-            APP_VERSION = "0.0.3"
+            //APP_VERSION = "0.0.3"
         }
     }
  }
